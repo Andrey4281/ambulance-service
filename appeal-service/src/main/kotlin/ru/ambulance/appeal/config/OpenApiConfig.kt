ba184f6d114a15702bffa3574353695290ca0ddb
Alexandr Leonov <as.leonov17@gmail.com>
2022-11-13 17:03:03 +0300

    Fix appeal swagger definition place

--- appeal-service/src/main/kotlin/ru/ambulance/appeal/config/OpenApiConfig.kt
+++ appeal-service/src/main/kotlin/ru/ambulance/appeal/config/OpenApiConfig.kt
@@ -1,6 +1,7 @@
 package ru.ambulance.appeal.config
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
@@ -8,6 +9,12 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@OpenAPIDefinition(info = io.swagger.v3.oas.annotations.info.Info(
+    title = "Swagger Appeal Service",
+    version = "1.0",
+    description = "Documentation APIs v1.0"
+)
+)
 class OpenApiConfig {
 
     @Bean
