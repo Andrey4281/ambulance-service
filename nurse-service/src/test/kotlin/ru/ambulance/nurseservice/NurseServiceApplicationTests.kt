b4c8fb8f2529c6fe41bbe3f5aa8f52c24d65bf2a
asemenov <asemenov@rtc.usetech.ru>
2022-12-06 13:37:26 -0500

    Issue: developing Doctor service
    In this commit:
    - Init nurse service

---
+++ nurse-service/src/test/kotlin/ru/ambulance/nurseservice/NurseServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.ambulance.nurseservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NurseServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
