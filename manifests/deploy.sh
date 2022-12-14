dfedb06b8f44190450b0e14843685e01c14f915b
asemenov <asemenov@rtc.usetech.ru>
2022-12-14 01:02:06 -0500

    Issue: deploy services
    In this commit:
    - Add deploy script

---
+++ manifests/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd kafka
+kubectl apply -f kafka-namespace.yaml
+helm -n kafka install kafka-cluster bitnami/kafka
+helm install -f cowl-values.yaml kowl cloudhut/kowl -n kafka
+cd ../appeal
+kubectl apply -f appeal-db-namespace.yaml
+kubectl apply -f appeal-db-secret.yaml
+helm -n appeal-db install appeal-database -f appeal-postgresql-values.yaml bitnami/postgresql
+cd ../doctor
+kubectl apply -f doctor-db-namespace.yaml
+kubectl apply -f doctor-db-secret.yaml
+helm -n doctor-db install doctor-database -f doctor-postgresql-values.yaml bitnami/postgresql
+cd ../nurse
+kubectl apply -f nurse-db-namespace.yaml
+kubectl apply -f nurse-db-secret.yaml
+helm -n nurse-db install nurse-database -f nurse-postgresql-values.yaml bitnami/postgresql
+cd ../helm
+helm install appeal-service backend
+helm install -f doctor-values.yaml doctor-service backend
+helm install -f nurse-values.yaml nurse-service backend
+cd ../gateway
+kubectl apply -f gateway-namespace.yaml
+kubectl apply -f gateway-config.yaml
+kubectl apply -f gateway-deployment.yaml
